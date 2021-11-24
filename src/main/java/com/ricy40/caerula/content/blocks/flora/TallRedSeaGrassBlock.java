bac8715a164788e3fa5115a08674423523f78d93
Ricy40 <60597171+Ricy40@users.noreply.github.com>
2021-11-24 21:57:31 +0000

    Red Sea Grass finished

---
+++ src/main/java/com/ricy40/caerula/content/blocks/flora/TallRedSeaGrassBlock.java
@@ -0,0 +1,20 @@
+package com.ricy40.caerula.content.blocks.flora;
+
+import com.ricy40.caerula.core.init.BlockInit;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.TallSeaGrassBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
+
+public class TallRedSeaGrassBlock extends TallSeaGrassBlock {
+
+    public TallRedSeaGrassBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public ItemStack getCloneItemStack(IBlockReader blockReader, BlockPos pos, BlockState state) {
+        return new ItemStack(BlockInit.RED_SEAGRASS.get());
+    }
+}
