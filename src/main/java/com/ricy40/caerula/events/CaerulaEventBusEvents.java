633b3594e36d580f44e4458cb6f12b29047fa04f
Ricy40 <60597171+Ricy40@users.noreply.github.com>
2021-11-28 21:43:57 +0000

    Lula Created

--- src/main/java/com/ricy40/caerula/events/CaerulaEventBusEvents.java
+++ src/main/java/com/ricy40/caerula/events/CaerulaEventBusEvents.java
@@ -1,10 +1,13 @@
 package com.ricy40.caerula.events;
 
+import com.ricy40.caerula.Caerula;
 import com.ricy40.caerula.entity.EntityTypeInit;
 import com.ricy40.caerula.entity.entities.LulaEntity;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod.EventBusSubscriber(modid = Caerula.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CaerulaEventBusEvents {
 
     @SubscribeEvent
